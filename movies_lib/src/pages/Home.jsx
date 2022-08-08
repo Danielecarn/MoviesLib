d6ed5e4e8ea97e77945da06f4a2c7bc835526982
Danielecarn <dannygoncalves41@gmail.com>
2022-08-07 23:23:06 -0300

    Pegando filmes mais votados da api

--- movies_lib/src/pages/Home.jsx
+++ movies_lib/src/pages/Home.jsx
@@ -1,7 +1,30 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+const moviesURL= import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
 
 const Home = () =>{
+
+    const [topMovies, setTopMovies] = useState([])
+
+    const getTopRatedMovies = async (url) => {
+        const res = await fetch(url);
+        const data = await res.json();
+
+        setTopMovies(data.results)
+    }
+
+    useEffect(()=>{
+
+        const topRatedUrl = `${moviesURL}top_rated?${apiKey}`;
+
+        getTopRatedMovies(topRatedUrl);
+
+    },[])
+
     return(
-        <div>Home</div>
+        <div>Home</div>    
     );
 }
 
