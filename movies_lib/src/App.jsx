898f46f01f59ebfcc6e25707ad95455345e5b9c1
Danielecarn <dannygoncalves41@gmail.com>
2022-08-07 22:30:19 -0300

    Link das rotas

--- movies_lib/src/App.jsx
+++ movies_lib/src/App.jsx
@@ -1,10 +1,18 @@
+import { Link, Outlet } from 'react-router-dom';
+
 import './App.css'
 
 function App() {
 
   return (
     <div className="App">
+      <nav id="navbar">
+        <h2>
+          <Link to="/">MoviesLib</Link>
+        </h2>
+      </nav>
       <h2>Movies Lib</h2>
+      <Outlet/>
     </div>
   )
 }
