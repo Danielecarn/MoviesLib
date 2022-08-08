24a3703a4477730ae05467b7a5d057922ddb4fe6
Danielecarn <dannygoncalves41@gmail.com>
2022-08-07 22:22:02 -0300

    Estrutura base dos componentes

--- movies_lib/src/main.jsx
+++ movies_lib/src/main.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
+import Home from './pages/Home';
+import Search from './pages/Search';
+import Movie from './pages/Movie';
+
 import App from './App'
 
 import './index.css'
